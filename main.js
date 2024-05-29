17ec49a09040055a3b2685d40c19ad2381b9b4ed
TheDiamonds559 <39632524+TheDiamonds559@users.noreply.github.com>
2024-05-29 15:30:56 +0100

    Made round logic!

--- main.js
+++ main.js
@@ -4,4 +4,29 @@ let getComputerChoice = () => options[Math.floor(Math.random() * 3)]
 
 let getHumanChoice = () => options[parseInt(prompt("0: rock, 1: paper, 2: scissors"))];
 
-console.log(getHumanChoice());
+let humanScore = computerScore = 0;
+
+let playRound = (humanChoice, computerChoice) => {
+    console.log("Computer: " + computerChoice);
+    console.log("human: " + humanChoice);
+    if (humanChoice == computerChoice)
+        {
+        console.log("Draw!");
+        }
+    else if (
+        (humanChoice == 'rock' && computerChoice == 'paper')     || 
+        (humanChoice == 'paper' && computerChoice == 'scissors') ||
+        (humanChoice == 'scissors' && computerChoice == 'rock')
+       )
+       {
+        console.log("Lose!");
+        computerScore++;
+       }
+    else {
+        console.log("Win!");
+        humanScore++;
+    }
+    console.log("Scores:\nHuman: " + humanScore + "\nComputer: " + computerScore);
+}
+
+playRound(getHumanChoice(), getComputerChoice());
