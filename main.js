e490d50fd5dfe186a9d8dc2108e96db92bf392a4
TheDiamonds559 <39632524+TheDiamonds559@users.noreply.github.com>
2024-05-29 15:13:39 +0100

    Implemented human logic

--- main.js
+++ main.js
@@ -2,3 +2,6 @@ let options = ['rock', 'paper', 'scissors'];
 
 let getComputerChoice = () => options[Math.floor(Math.random() * 3)]
 
+let getHumanChoice = () => options[parseInt(prompt("0: rock, 1: paper, 2: scissors"))];
+
+console.log(getHumanChoice());
